2ae170968c3139810ee657bf71ded475a6f72ea8
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-08-11 20:24:49 -0400

    Finished configuring final endpoint

--- edx-express-app/readme.txt
+++ edx-express-app/readme.txt
@@ -1,3 +1,21 @@
 This project is based off of edx's "Microsoft: DEV280x Building Functional Prototypes using Node.js"
 
 It's a simple express server with endpoints to manage a sqlite3 database.
+
+To execute the post endpoint, use Postman, or the REST client of your choice, and
+build an API POST request with the following configuration:
+
+request URL: localhost:3000/quotes
+request method: POST
+request body:
+    encoding format: x-www-form-urlencoded
+    quote: "Sample Quote"
+    author: "Sample Author"
+    year: 1940
+
+A powershell example of such is as follows:
+> $postParams = @{quote='Sample Quote';author='Sample Author';year=1940}
+> $postQuoteInvocation = Invoke-WebRequest -Uri http://localhost:3000/quotes -method POST
+    -Body $postParams -ContentType 'application/x-www-form-urlencoded'
+
+(Note that the default ContentType already is 'application/x-www-form-urlencoded')
--- edx-express-app/server.js
+++ edx-express-app/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 // Configure sqlite3
-const db = new sqlite3.Database('quotes.db');
+const db = new sqlite3.Database('quotes.sqlite');
 
 
 // app.use appears to be a standard method for defining express middleware, of which this is a lot
@@ -19,21 +19,59 @@ app.get('/', (request, response) => {
 
 app.get('/quotes', (request, response) => {
 	if (request.query.year) {
-		response.send(`Get quotes with year === ${request.query.year}`);
+		console.log(`Get quotes with year === ${request.query.year}`);
+	    db.all('SELECT * FROM quotes WHERE year = ?',
+			[request.query.year], (err, rows) => {
+	    		if (err) response.send(err.message);
+				else response.json(rows);
+			});
 	} else {
-	    response.send("Get all quotes, since no year was specified");
+	    console.log("Get all quotes, since no year was specified");
+	    db.all('SELECT * FROM quotes', (err, rows) => {
+	    	if (err) response.send(err.message);
+	    	else response.json(rows);
+		});
 	}
 });
 
 app.get('/quotes/:id', (request, response) => {
-	response.send(`View a quote with the id ${request.params.id}`);
+	console.log(`View a quote with the id ${request.params.id}`);
+	db.get('SELECT * FROM quotes WHERE ROWID = ?',
+		[request.params.id], (err, row) => {
+			if (err) response.send(err.message);
+			else response.json(row);
+		});
 });
 
 app.post('/quotes', (request, response) => {
 	console.log(`Insert a new quote: ${request.body.quote}`);
+	db.run('INSERT INTO Quotes VALUES (?, ?, ?)',
+		[request.body.quote, request.body.author, request.body.year],
+		(err) => {
+			if (err) response.send(err);
+			else {
+			    // this is referring to the database?
+				response.send('Inserted quote with id: ' + this.lastID);
+			}
+		});
 	response.json(request.body);
 });
 
 app.listen(port, () => {
 	console.log("Express app listening on port " + port);
 });
+
+/**
+ * Prints out all the quotes into the console.
+ */
+function printQuotes() {
+	db.serialize(() => {
+		db.each('SELECT * FROM Quotes', (err, row) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(row);
+			}
+		});
+	});
+}
