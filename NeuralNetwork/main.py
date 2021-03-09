152f9b0983bd5b19b469fea83274be4f5b7bccbd
JoshDesmond <JoshDesmond@users.noreply.github.com>
2021-03-09 17:44:30 -0500

    Testing some python stuff

---
+++ NeuralNetwork/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy
+print('numpy: %s' % numpy.__version__)
+
+
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
