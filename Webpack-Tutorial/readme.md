91828a5d0739ca07608ff78591e1983ea51529ff
JoshDesmond <JoshDesmond@users.noreply.github.com>
2021-03-11 12:37:34 -0500

    Switched js to use ES6 modules and use webpack

--- Webpack-Tutorial/readme.md
+++ Webpack-Tutorial/readme.md
@@ -1,3 +1,5 @@
 This repo was built by following freeCodeCamp.org's Webpack tutorial. See:
+
 https://www.youtube.com/watch?v=MpGLUVbqoYQ
+
 and https://github.com/Colt/webpack-demo-app
--- Webpack-Tutorial/src/index.js
+++ Webpack-Tutorial/src/index.js
@@ -1,1 +1,7 @@
-alert("HELLO FROM WEBPACK!");
+import { run } from "./app/app.js"
+import { ComponentService } from "./app/component.service.js"
+import { AlertService } from "./app/alert.service.js"
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
