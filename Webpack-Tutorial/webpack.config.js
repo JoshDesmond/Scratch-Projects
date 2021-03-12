5e12a4d012c3f6001f34fc2d163208d9af72ae43
JoshDesmond <JoshDesmond@users.noreply.github.com>
2021-03-12 13:16:16 -0500

    file reorgnization

--- Webpack-Tutorial/webpack.config.js
+++ Webpack-Tutorial/webpack.config.js
@@ -3,9 +3,17 @@ const path = require("path");
 module.exports = {
     mode: "development", // Doesn't minify
     devtool: "none", // Removes the eval wrapping for more legible code in build
-    entry: "./src/index.js",
+    entry: "./app/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["css-loader"]
+            }
+        ]
     }
 }
