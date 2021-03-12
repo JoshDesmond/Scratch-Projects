aa74050077ae46eb8797a18f0f009f5c542448c9
JoshDesmond <JoshDesmond@users.noreply.github.com>
2021-03-12 17:59:27 -0500

    Set up template extension to almost work

--- ManifestV3/webpack.config.js
+++ ManifestV3/webpack.config.js
@@ -1,21 +1,30 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
 module.exports = {
     mode: "development", // Doesn't minify
+    devtool: false,
     entry: {
         content: path.join(__dirname, "app", "js", "content.js"),
-        background: path.join(__dirname, "app", "js", "background.js")
+        background: path.join(__dirname, "app", "js", "background.js"),
+        options: path.join(__dirname, "app", "js", "options.js")
     },
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "[name].bundle.js"
     },
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "app/img", to: "img" },
+                { from: path.resolve(__dirname, "app/manifest.json") },
+                { from: path.resolve(__dirname, "app/options.html") },
+                { from: "app/_locales", to: "_locales" }
+            ]
+        })
+    ]
 }
