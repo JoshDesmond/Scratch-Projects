5c01356068a9dfc3aecc4e3f9e7ba53c6ef4169b
JoshDesmond <JoshDesmond@users.noreply.github.com>
2021-06-30 16:22:29 -0400

    Old mutation observer work or something

--- mutation-observer/index.js
+++ mutation-observer/index.js
@@ -4,4 +4,56 @@ function addContentToPage() {
     document.body.appendChild(p);
 }
 
-// function mutationCallback;
+function alterPageContent() {
+    const el = document.querySelector('p');
+    el.innerText += ".";
+}
+
+/**
+ *
+ *
+ * @param {sequence<MutationRecord>} mutations
+ * @param {MutationObserver} observer
+ */
+function mutationCallback(mutations, observer) {
+    for (let mut of mutations) {
+        console.log(mut);
+    }
+
+    console.log(observer);
+}
+
+
+
+/**
+ interface MutationObserver {
+  constructor(MutationCallback callback);
+  undefined observe(Node target, optional MutationObserverInit options = {});
+  undefined disconnect();
+  sequence<MutationRecord> takeRecords();
+};
+ callback MutationCallback = undefined (sequence<MutationRecord> mutations, MutationObserver observer);
+ dictionary MutationObserverInit {
+  boolean childList = false;
+  boolean attributes;
+  boolean characterData;
+  boolean subtree = false;
+  boolean attributeOldValue;
+  boolean characterDataOldValue;
+  sequence<DOMString> attributeFilter;
+};
+ */
+
+/** {@type MutationObserverInit} */
+const options = {
+    childList: true,
+    attributes: false,
+    characterData: true,
+    subtree: false,
+    attributeOldValue: false,
+    characterDataOldValue: false,
+    //attributeFilter: null // sequence<DOMString>
+};
+
+mutationObserver = new MutationObserver(mutationCallback);
+mutationObserver.observe(document.body, options);
