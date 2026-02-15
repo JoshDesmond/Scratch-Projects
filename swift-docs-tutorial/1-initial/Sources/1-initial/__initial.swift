bf8419437e80bfdafab00d66632f8b4aec0e9a10
JoshDesmond <JoshDesmond@users.noreply.github.com>
2026-02-15 11:02:15 -0500

    Adds swift tutorial project dir

---
+++ swift-docs-tutorial/1-initial/Sources/1-initial/__initial.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct __initial {
+    static func main() {
+        print("Hello, world!")
+
+        let testFloat: Float = 4
+        print(testFloat)
+    }
+}
